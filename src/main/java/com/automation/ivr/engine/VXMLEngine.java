705bd3f7bfbd84c0898421e96dfe90341a8fc8d9
Chandrashekar S <chandrashekar.s@orbitz.com>
2016-03-28 07:04:52 -0500

    IO Handler to track output and get input from user

--- src/main/java/com/automation/ivr/engine/VXMLEngine.java
+++ src/main/java/com/automation/ivr/engine/VXMLEngine.java
@@ -2,20 +2,21 @@ package com.automation.ivr.engine;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.Properties;
 
 import com.automation.ivr.exception.FileParsingException;
+import com.automation.ivr.io.IOHandler;
 import com.automation.ivr.parser.VXMLFileParser;
 import com.automation.ivr.tags.Tag;
 
 public class VXMLEngine {
 
     private File file;
-    public static  Properties properties;
+    private static boolean onMute;
+    private static IOHandler ioHandler;
     static {
-        properties = new Properties();
+        onMute = false;
+        ioHandler = new IOHandler();
     }
 
     public void main(String[] args) {
@@ -26,11 +27,10 @@ public class VXMLEngine {
          * vxmlFileParser.parse();
          */
 
-//        file = new File("examples/prompt_example.vxml");
+        // file = new File("examples/prompt_example.vxml");
         try {
             File propertiesFile = new File("ivr-automation.properties");
             FileInputStream fileInput = new FileInputStream(propertiesFile);
-            properties.load(fileInput);
             fileInput.close();
 
             List<Tag> tagList = vxmlFileParser.parse(file);
@@ -51,6 +51,22 @@ public class VXMLEngine {
 
     public void setFile(File file) {
         this.file = file;
+    }
+
+    public static boolean isOnMute() {
+        return onMute;
+    }
+
+    public static void setOnMute(boolean onMute) {
+        VXMLEngine.onMute = onMute;
+    }
+
+    public static IOHandler getIoHandler() {
+        return ioHandler;
+    }
+
+    public static void setIoHandler(IOHandler ioHandler) {
+        VXMLEngine.ioHandler = ioHandler;
     }
 
 }
